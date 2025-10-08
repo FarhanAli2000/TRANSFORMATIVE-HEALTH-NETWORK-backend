bcf3e3e61a1ae6880e2a40feed6bfb276a8f468f
Syed Farhan Ali <tofarhanali@gmail.com>
2025-10-08 14:20:23 +0500

    password change successfully

--- routes/auth.js
+++ routes/auth.js
@@ -102,6 +102,17 @@ router.post("/login", async (req, res) => {
 });
 
 // GET /api/auth/users/:id
+router.get("/users/:id", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ msg: "User not found" });
+
+    res.json({ user });
+  } catch (err) {
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
 router.get("/admin/user/:id", authMiddleware, async (req, res) => {
   try {
     const user = await User.findById(req.params.id).select("-password");
@@ -110,11 +121,12 @@ router.get("/admin/user/:id", authMiddleware, async (req, res) => {
     }
     res.json(user);
   } catch (err) {
-    console.error("Error fetching user:", err.message);
+    console.error(err.message);
     res.status(500).json({ msg: "Server error" });
   }
 });
 
+module.exports = router;
 
 // ===== Forgot Password =====
 router.post("/forgot-password", async (req, res) => {
